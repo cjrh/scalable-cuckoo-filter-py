274724e62bc1ebef95ea2eaa3693c435ed916d89
Caleb Hattingh <caleb.hattingh@gmail.com>
2024-06-28 16:23:12 +0200

    Simpler name, got building

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,53 @@
 use pyo3::prelude::*;
 use rand::Rng;
+use scalable_cuckoo_filter::ScalableCuckooFilter;
 use std::cmp::Ordering;
 use std::io;
+
+// Using pyo3, let's make a wrapper class for the `scalable_cuckoo_filter::ScalableCuckooFilter`
+// struct. This will allow us to create a Python class that wraps the Rust struct.
+// The documentation is here:
+// https://docs.rs/scalable_cuckoo_filter/0.3.2/scalable_cuckoo_filter/struct.ScalableCuckooFilter.html
+#[pyclass(unsendable)]
+pub(crate) struct PyScalableCuckooFilter {
+    inner: ScalableCuckooFilter<String>,
+}
+
+/*
+
+#[pyclass(frozen, module = "tantivy.tantivy")]
+pub(crate) struct Query {
+    pub(crate) inner: Box<dyn tv::query::Query>,
+}
+
+*/
+
+// Implement the Python class methods for the `PyScalableCuckooFilter` class.
+// This will allow us to create a Python class that wraps the Rust struct.
+#[pymethods]
+impl PyScalableCuckooFilter {
+    #[new]
+    fn new(initial_capacity_hint: usize, false_positive_probability: f64) -> Self {
+        PyScalableCuckooFilter {
+            inner: ScalableCuckooFilter::new(
+                initial_capacity_hint,
+                false_positive_probability,
+            ),
+        }
+    }
+
+    // fn insert(&mut self, item: u64) -> bool {
+    //     self.inner.insert(item)
+    // }
+
+    // fn contains(&self, item: u64) -> bool {
+    //     self.inner.contains(item)
+    // }
+
+    // fn remove(&mut self, item: u64) -> bool {
+    //     self.inner.remove(item)
+    // }
+}
 
 #[pyfunction]
 fn guess_the_number() {
@@ -40,7 +86,8 @@ fn guess_the_number() {
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
-fn scalable_cuckoo_filter(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn scf(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<PyScalableCuckooFilter>()?;
     m.add_function(wrap_pyfunction!(guess_the_number, m)?)?;
 
     Ok(())
